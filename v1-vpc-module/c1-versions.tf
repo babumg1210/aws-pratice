518a0b3f7eef92e056e838ece496e94001ec2d5d
babumg1210 <babumg1210@outlook.com>
2025-02-05 22:19:10 +0530

    Update c1-versions.tf

--- v1-vpc-module/c1-versions.tf
+++ v1-vpc-module/c1-versions.tf
@@ -7,6 +7,12 @@ terraform {
       version = ">= 5.0"  
     }
   }
+   # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraform-on-aws-for-ec2"
+    key    = "dev/project1-vpc/terraform.tfstate"
+    region = "us-east-1"     
+  }  
 }
 
 # Provider Block
