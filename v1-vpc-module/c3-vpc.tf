aaec05f9365e43a49e5acf9a97a592951ed3ae3a
babumg1210 <babumg1210@outlook.com>
2025-02-06 20:27:52 +0530

    Update c3-vpc.tf

--- v1-vpc-module/c3-vpc.tf
+++ v1-vpc-module/c3-vpc.tf
@@ -78,5 +78,25 @@ resource "aws_route_table" "private_rt" {
     Name = "private-rt"
   }
 }
+resource "aws_security_group" "app_sg" {
+  vpc_id = aws_vpc.main.id
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-sg"
+  }
+}
 
