e9f245bc4f8f77155356bcd4e416c2bc0746451a
babumg1210 <babumg1210@outlook.com>
2025-02-06 17:44:51 +0530

    Update c3-vpc.tf

--- v1-vpc-module/c3-vpc.tf
+++ v1-vpc-module/c3-vpc.tf
@@ -5,3 +5,30 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.2.0/24"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.3.0/24"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
