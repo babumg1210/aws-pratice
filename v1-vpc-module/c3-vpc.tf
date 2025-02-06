54ac3b43eaf73fbc630881758dd40ad484979c8a
babumg1210 <babumg1210@outlook.com>
2025-02-06 18:26:44 +0530

    Update c3-vpc.tf

--- v1-vpc-module/c3-vpc.tf
+++ v1-vpc-module/c3-vpc.tf
@@ -55,3 +55,15 @@ resource "aws_route_table_association" "public_subnet_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
