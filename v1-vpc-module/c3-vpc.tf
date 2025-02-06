ad95d1e16d35742f7920fc0c7f8c272245ce3eae
babumg1210 <babumg1210@outlook.com>
2025-02-06 18:26:05 +0530

    Update c3-vpc.tf

--- v1-vpc-module/c3-vpc.tf
+++ v1-vpc-module/c3-vpc.tf
@@ -51,3 +51,7 @@ resource "aws_route_table" "public_rt" {
     Name = "public-rt"
   }
 }
+resource "aws_route_table_association" "public_subnet_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
