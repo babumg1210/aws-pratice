db3734319de45aa9786f8447eb54aaf48feb6951
babumg1210 <babumg1210@outlook.com>
2025-02-11 15:30:01 +0530

    Update c4-ami-datasource.tf

--- v1-vpc-module/c4-ami-datasource.tf
+++ v1-vpc-module/c4-ami-datasource.tf
@@ -19,3 +19,28 @@ data "aws_ami" "amzlinux2" {
     values = ["x86_64"]
   }
 }
+# Get List of Availability Zones in a Specific Region
+# Region is set in c1-versions.tf in Provider Block
+# Datasource-1
+data "aws_availability_zones" "my_azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+# Check if that respective Instance Type is supported in that Specific Region in list of availability Zones
+# Get the List of Availability Zones in a Particular region where that respective Instance Type is supported
+# Datasource-2
+data "aws_ec2_instance_type_offerings" "my_ins_type" {
+  for_each = toset(data.aws_availability_zones.my_azones.names)
+  filter {
+    name   = "instance-type"
+    values = ["t3.micro"]
+  }
+  filter {
+    name   = "location"
+    values = [each.key]
+  }
+  location_type = "availability-zone"
+}
